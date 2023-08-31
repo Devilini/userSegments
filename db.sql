de85eb42c59f98e9e8599412255bdbb94fb94b09
Devilini <devilini27@mail.ru>
2023-09-01 01:47:44 +0300

    Fix readme

--- db.sql
+++ db.sql
@@ -2,14 +2,14 @@ CREATE TABLE IF NOT EXISTS users
 (
     id   serial PRIMARY KEY,
     name varchar(255)  not null
-)
+);
 
 CREATE TABLE IF NOT EXISTS segments
 (
     id   serial PRIMARY KEY,
-    slug varchar(255)  not null,
+    slug varchar(255) not null,
     UNIQUE (slug)
-)
+);
 
 
 CREATE TABLE IF NOT EXISTS user_segments
@@ -22,12 +22,16 @@ CREATE TABLE IF NOT EXISTS user_segments
     PRIMARY KEY(user_id, segment_id)
 );
 
+CREATE TYPE segment_operation AS ENUM('add', 'delete');
+
 CREATE TABLE IF NOT EXISTS segments_history
 (
     id   serial PRIMARY KEY,
-    user_id int,
-    segment_id int,
-    operation varchar(20),
-    created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+    user_id int not null,
+    segment_id int not null,
+    operation segment_operation not null,
+    created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    foreign key (user_id) references users (id),
+    foreign key (segment_id) references segments (id)
 );
 
